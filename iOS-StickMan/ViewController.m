ebb19445dbb070458df438bba88d1776b026aef2
Abhijit Joshi <joshi1974@gmail.com>
2014-04-28 03:15:55 -0400

    added several UIImageView layers on top of each other

--- iOS-StickMan/ViewController.m
+++ iOS-StickMan/ViewController.m
@@ -13,12 +13,20 @@
 - (IBAction)drawSadMan:(id)sender;
 - (IBAction)toggleHat:(id)sender;
 @property (weak, nonatomic) IBOutlet UIImageView *displayImage;
+@property (weak, nonatomic) IBOutlet UIImageView *layer1;
+@property (weak, nonatomic) IBOutlet UIImageView *layer2;
+@property (weak, nonatomic) IBOutlet UIImageView *layer3;
+@property (weak, nonatomic) IBOutlet UIImageView *layer4;
 
 @end
 
 @implementation ViewController
 
 @synthesize displayImage;
+@synthesize layer1;
+@synthesize layer2;
+@synthesize layer3;
+@synthesize layer4;
 
 - (void)viewDidLoad
 {
@@ -34,9 +42,18 @@
 
 - (IBAction)drawHappyMan:(id)sender {
     [displayImage setImage:[UIImage imageNamed: @"baseline.png"]];
+    [layer1       setImage:[UIImage imageNamed: @"happy.png"]];
+    [layer2       setImage:[UIImage imageNamed: @"legs.png"]];
+    [layer3       setImage:[UIImage imageNamed: @"leftHandUp.png"]];
+    [layer4       setImage:[UIImage imageNamed: @"rightHandUp.png"]];
 }
 
 - (IBAction)drawSadMan:(id)sender {
+    [displayImage setImage:[UIImage imageNamed: @"baseline.png"]];
+    [layer1       setImage:[UIImage imageNamed: @"sad.png"]];
+    [layer2       setImage:[UIImage imageNamed: @"legs.png"]];
+    [layer3       setImage:[UIImage imageNamed: @"leftHandDown.png"]];
+    [layer4       setImage:[UIImage imageNamed: @"rightHandDown.png"]];
 }
 
 - (IBAction)toggleHat:(id)sender {
